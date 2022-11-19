ece908507f09445fb0e8edc419ff771c6acd2301
Andrew Parker <andrew.parker@beslogic.com>
2022-11-19 18:03:52 -0500

    first commit

---
+++ apps/angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-generatorz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'angular';
+}
