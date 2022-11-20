3ec6e9747d1e76eef18aeb2217953add4f229d54
Andrew Parker <andrew.parker@beslogic.com>
2022-11-19 21:45:18 -0500

    Adding the api as an extension for the ddd angular

---
+++ libs/ddd-angular/src/generators/ddd-api/generator.ts
@@ -0,0 +1,13 @@
+import {
+  Tree,
+} from '@nrwl/devkit';
+import { DddApiGeneratorSchema } from './schema';
+import dddGenerator from '@angular-architects/ddd/src/generators/api';
+
+export default async function (tree: Tree, options: DddApiGeneratorSchema) {
+  await dddGenerator(tree, {
+    ...options,
+    type: 'buildable',
+    standalone: false,
+  })
+}
---
+++ libs/ddd-angular/src/generators/ddd-api/schema.d.ts
@@ -0,0 +1,4 @@
+export interface DddApiGeneratorSchema {
+    name: string;
+    domain: string;
+}
