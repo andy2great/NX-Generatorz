2d6eb5587a8252a207f4638db0b7fa318020df58
Andrew Parker <andrew.parker@beslogic.com>
2022-11-19 21:39:45 -0500

    Adding ddd util

---
+++ libs/ddd-angular/src/generators/ddd-util/generator.ts
@@ -0,0 +1,14 @@
+import {
+  Tree,
+} from '@nrwl/devkit';
+import { DddUtilGeneratorSchema } from './schema';
+import dddGenerator from '@angular-architects/ddd/src/generators/util';
+
+export default async function (tree: Tree, options: DddUtilGeneratorSchema) {
+  await dddGenerator(tree, {
+    ...options,
+    type: 'buildable',
+    shared: true,
+    standalone: false
+  })
+}
