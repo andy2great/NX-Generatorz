4e1631898a85b5cb94f31b8319d3a7d12665089a
Andrew Parker <andrew.parker@beslogic.com>
2022-11-19 21:24:39 -0500

    adding feature generator

---
+++ libs/ddd-angular/src/generators/ddd-feature/generator.ts
@@ -0,0 +1,28 @@
+import {
+  Tree,
+} from '@nrwl/devkit';
+import { DddFeatureGeneratorSchema } from './schema';
+import dddFeatureGenerator from '@angular-architects/ddd/src/generators/feature';
+import { applicationGenerator } from '@nrwl/angular/generators'
+import { removeGenerator } from '@nrwl/workspace';
+
+const APP_NAME = 'bobby-john-boratos';
+
+export default async function (tree: Tree, options: DddFeatureGeneratorSchema) {
+  await applicationGenerator(tree, {
+    name: APP_NAME,
+    unitTestRunner: 'none' as any,
+    e2eTestRunner: 'none' as any
+  });
+  await dddFeatureGenerator(tree, {
+    ...options,
+    app: APP_NAME,
+    standalone: false,
+    entity: options.name,
+  });
+  await removeGenerator(tree, {
+    projectName: APP_NAME,
+    forceRemove: false,
+    skipFormat: false
+  })
+}
---
+++ libs/ddd-angular/src/generators/ddd-feature/schema.d.ts
@@ -0,0 +1,4 @@
+export interface DddFeatureGeneratorSchema {
+    name: string;
+    domain: string;
+}
