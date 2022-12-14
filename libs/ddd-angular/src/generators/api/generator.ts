54aa820963a017268e1e7c2b07fd0613ffb3c677
andy2great <great2g@hotmail.com>
2022-12-14 12:49:38 -0500

    Fixing bug where the feature, api and shell would be added outside of the domain (#19)
    * Fixing bug where the feature, api and shell would be added outside of the domain
    
    * What is a test?
    
    * test? uh?
    
    * Removing anything related to the tests
    
    Co-authored-by: Andrew Parker <andrew.parker@beslogic.com>

--- libs/ddd-angular/src/generators/api/generator.ts
+++ libs/ddd-angular/src/generators/api/generator.ts
@@ -1,12 +1,15 @@
 import { Tree } from '@nrwl/devkit';
 import { DddApiGeneratorSchema } from './schema';
 import dddGenerator from '@angular-architects/ddd/src/generators/api';
-import { guardValidDomain } from '../../helpers';
+import { domainNameFromProject, guardValidDomain } from '../../helpers';
 
 export default async function (tree: Tree, options: DddApiGeneratorSchema) {
   guardValidDomain(tree, options.domain);
+  const domainName = domainNameFromProject(tree, options.domain);
+  
   await dddGenerator(tree, {
     ...options,
+    domain: domainName,
     type: 'buildable',
     standalone: false,
   });
