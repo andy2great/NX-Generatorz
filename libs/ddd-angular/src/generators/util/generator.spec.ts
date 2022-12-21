03f99351d8d1c695e33735af68a38c491b3635a3
Matthieu Pauze <matthieu.pauze@beslogic.com>
2022-12-21 13:46:20 -0500

    add tests for util generator

--- libs/ddd-angular/src/generators/util/generator.spec.ts
+++ libs/ddd-angular/src/generators/util/generator.spec.ts
@@ -3,7 +3,12 @@ import { Tree, readProjectConfiguration } from '@nrwl/devkit';
 
 import generator from './generator';
 import { DddUtilGeneratorSchema } from './schema';
-import { changeIs } from '../../helpers/test-helper';
+import {
+  changeIs,
+  generalProjectFiles,
+  generalTestingFiles,
+  nxFiles,
+} from '../../helpers/test-helper';
 
 describe('util generator', () => {
   let appTree: Tree;
@@ -17,7 +22,10 @@ describe('util generator', () => {
   });
 
   it('should generate a shared util', async () => {
-    const config = readProjectConfiguration(appTree, 'shared-util-test');
+    const config = readProjectConfiguration(
+      appTree,
+      `shared-util-${options.name}`
+    );
     expect(config).toBeDefined();
   });
 
@@ -27,5 +35,34 @@ describe('util generator', () => {
       .find((change) => changeIs(change, 'readme.md'));
 
     expect(readme).toBeUndefined();
+  });
+
+  it('should contain base NX files', () => {
+    const changes = appTree.listChanges().map((change) => change.path);
+
+    nxFiles.forEach((expectedFile) => {
+      expect(changes).toContain(expectedFile);
+    });
+  });
+
+  it('should contain general project files', () => {
+    const changes = appTree.listChanges().map((change) => change.path);
+
+    generalProjectFiles(
+      `shared-util-${options.name}`,
+      `shared/util-${options.name}`
+    ).forEach((expectedFile) => {
+      expect(changes).toContain(expectedFile);
+    });
+  });
+
+  it('should contain testing files', () => {
+    const changes = appTree.listChanges().map((change) => change.path);
+
+    generalTestingFiles(`shared/util-${options.name}`).forEach(
+      (expectedFile) => {
+        expect(changes).toContain(expectedFile);
+      }
+    );
   });
 });
